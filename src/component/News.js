d3016507e19c8c5da6949894fd8c6a6e9f367071
jayeshpatel882006 <124601438+jayeshpatelswami@users.noreply.github.com>
2023-02-19 11:44:54 +0530

    fifth

--- src/component/News.js
+++ src/component/News.js
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import Newsitem from "./Newsitem";
 import Loading from "./Loading";
 import PropTypes from "prop-types"
+import InfiniteScroll from "react-infinite-scroll-component";
 
 export default class News extends Component {
   
@@ -9,6 +10,7 @@ export default class News extends Component {
     contry: "in",
     pagesize: 5,
     category: "general"
+    
   }
 
   static propTypes ={
@@ -23,47 +25,75 @@ export default class News extends Component {
     this.state = {
       articles:[] ,
       loading: false,
-      page: 1
-      
+      page: 1,
+      totalresult: 0  
       };
 
     }
 
     
     async componentDidMount(){
-      let url = `https://newsapi.org/v2/top-headlines?country=${this.props.contry}&category=${this.props.category}&apiKey=1cfbe18de4d94760b8a2ddd33b2d89f6&page-1&pagesize=${this.props.pagesize}`;
+      let url = `https://newsapi.org/v2/top-headlines?country=${this.props.contry}&category=${this.props.category}&apiKey=1cfbe18de4d94760b8a2ddd33b2d89f6&page=${this.props.page}&pagesize=${this.props.pagesize}`;
       // alert(url);
+      this.setState({loading: true})
       let data = await fetch(url);
       let parsedata = await data.json()
-      // console.log(this.props.category);
-  //  console.log(parsedata);
-  this.setState({articles: parsedata.articles})
+      this.setState({
+        articles: parsedata.articles,
+        loading: false,
+        totalresult: parsedata.totalresult
+      })  
 }
-  
+
+ fatchmoredata = async () => {
+  this.setState({ page: this.state.page + 1})
+    // alert(this.state.page);
+      // const url = `https://newsapi.org/v2/top-headlines?country=${this.props.contry}&category=${this.props.category}&apiKey=1cfbe18de4d94760b8a2ddd33b2d89f6&page=2&pagesize=${this.props.pagesize}`;
+         const url = `https://newsapi.org/v2/top-headlines?country=${this.props.contry}&category=${this.props.category}&apiKey=1cfbe18de4d94760b8a2ddd33b2d89f6&page=${this.state.page}&pagesize=${this.props.pagesize}`;
+      // alert(url);
+      this.setState({loading: true})
+      let data = await fetch(url);
+      let parsedata = await data.json()
+      this.setState({
+        articles: this.state.articles.concat(parsedata.articles),
+        totalresult: parsedata.totalresult ,
+        loading: false ,
+       })
+
+};
+
 
   render() {
     return (
       <div className="container">
              
                 <h1 className="text-center my-4">Patel's Akhbar</h1>
-          {this.state.loading  &&   <Loading/>}
+          {/* {this.state.loading  &&   <Loading/>} */}
+
+         <InfiniteScroll
+         dataLength={this.state.articles.length}
+         next={this.fatchmoredata}
+         hasMore={this.state.articles.length !== this.state.totalresult}
+         loader={<Loading/>}
+         >
+
+             <div className="container">
            <div className="row">
           {this.state.articles.map((elme) => {
-            return ( 
-              <div className="col-md-3" key={elme.url}>
-                  <div key="x">
+             return  <div className="col-md-3" key={elme.url}>
+                  
                 <Newsitem 
                  title1={elme.title ? elme.title.slice(0,35) : ""}
                  discription1={elme.description ?  elme.description.slice(0,55) : ""} 
                  url={elme.url}
-                 
                  imgurl={elme.urlToImage}/>               
-                 </div>
+              
               </div>
-
-            );
-          })}
+          }
+          )}
           </div>
+          </div>
+          </InfiniteScroll>
        </div>
      
      
